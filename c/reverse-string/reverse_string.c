555a54947097514f216f599d8f0577062aee8c97
Samuel Cristobal <el.tobal@gmail.com>
2024-04-30 07:50:20 +0200

    Reverse string, copy everything

--- c/reverse-string/reverse_string.c
+++ c/reverse-string/reverse_string.c
@@ -1,1 +1,21 @@
 #include "reverse_string.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *reverse(const char *value) {
+    int n = strlen(value);
+
+    char *r = malloc(n);
+
+    int j;
+
+    for (j = 0; j < n; j++) {
+        r[j] = value[n - 1 - j];
+    }
+
+    r[n] = '\0';
+
+    return r;
+}
